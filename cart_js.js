f0695b09173250de5c0a9f9b2a1e1024391d13a5
Nishant Sachan <mr.nishantsachan@gmail.com>
2024-12-13 23:40:19 +0530

    first commit

---
+++ cart_js.js
@@ -0,0 +1,18 @@
+function one()
+{
+    var a=f.t2.value;
+    var b=f.t3.value;
+    f.a1.value=a*b;
+}
+function two()
+{
+    var a=f.t5.value;
+    var b=f.t6.value;
+    f.a2.value=a*b;
+}
+function add()
+{
+var s1=parseInt(f.a1.value);
+var s2=parseInt(f.a2.value);
+f.a3.value=s1+s2;
+}
